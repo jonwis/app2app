d341dfa0e11136c25aab639b6203f141105b1e2f
Jon Wiswall <jonwis@microsoft.com>
2023-05-22 13:24:32 -0700

    Reference WIL

--- App2AppCore/pch.h
+++ App2AppCore/pch.h
@@ -1,4 +1,7 @@
 ﻿#pragma once
 #include <unknwn.h>
+#include <wil/cppwinrt.h>
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.ApplicationModel.AppExtensions.h>
+#include <winrt/Windows.Management.Deployment.h>
