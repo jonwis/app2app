2ff6ae1b2d776e48b50a548dc8fcee2bed00d8e4
Jon Wiswall <jonwis@microsoft.com>
2023-05-22 13:23:37 -0700

    Add corrected output types

--- App2AppCore/App2AppCallResult.cpp
+++ App2AppCore/App2AppCallResult.cpp
@@ -1,15 +1,3 @@
 #include "pch.h"
 #include "App2AppCallResult.h"
 #include "App2AppCallResult.g.cpp"
-
-namespace winrt::App2App::implementation
-{
-    App2AppCallResult::App2AppCallResult(winrt::hresult const& error, winrt::Windows::Foundation::IInspectable const& result)
-    {
-        throw hresult_not_implemented();
-    }
-    winrt::Windows::Foundation::IInspectable App2AppCallResult::Result()
-    {
-        throw hresult_not_implemented();
-    }
-}
--- App2AppCore/App2AppCallResult.h
+++ App2AppCore/App2AppCallResult.h
@@ -5,10 +5,31 @@ namespace winrt::App2App::implementation
 {
     struct App2AppCallResult : App2AppCallResultT<App2AppCallResult>
     {
-        App2AppCallResult() = default;
+        App2AppCallResult(App2AppCallResultStatus status, winrt::hresult const& error, winrt::Windows::Foundation::IInspectable const& result) :
+            m_status(status),
+            m_error(error),
+            m_result(result)
+        {
+        }
 
-        App2AppCallResult(winrt::hresult const& error, winrt::Windows::Foundation::IInspectable const& result);
-        winrt::Windows::Foundation::IInspectable Result();
+        winrt::Windows::Foundation::IInspectable Result()
+        {
+            return m_result;
+        }
+
+        App2AppCallResultStatus Status()
+        {
+            return m_status;
+        }
+
+        winrt::hresult ExtendedError()
+        {
+            return m_error;
+        }
+
+        App2AppCallResultStatus m_status;
+        winrt::hresult m_error;
+        winrt::Windows::Foundation::IInspectable m_result;
     };
 }
 namespace winrt::App2App::factory_implementation
